6e4108f9398f2cf21a8a0dfa0f6c588190e63567
slartibartfast <vrashabh86@gmail.com>
2016-11-19 12:35:06 +0530

    First attempt to breakdown an image into 1024 parts

---
+++ app/src/main/java/com/example/slartibartfast/mospho/Activities/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.slartibartfast.mospho.Activities;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+
+
+import com.example.slartibartfast.mospho.Adapters.ImageAdapter;
+import com.example.slartibartfast.mospho.R;
+
+import java.util.ArrayList;
+
+/*
+MosPho: Display a simple interface at launch.  One button that the user uses to select an image from the
+Android Gallery.  Create a mosaic based on the chosen image and save it.
+The mosaic is saved into a file locally(on the phone) and inserted into the Android Gallery.
+*/
+
+public class MainActivity extends AppCompatActivity {
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+
+    /**
+     * Good ol' OnCreate
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
+        //Simple button to select an image from the device
+        Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+
+    /**
+     * First Operation:
+     * Open the Android gallery and let the user choose an image. A mosaic will be
+     * created based on the chosen image, saved and inserted to the gallery.
+     * The mosaic will also displayed on the screen.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            // Create a mutable Bitmap
+            Bitmap originalImage = BitmapFactory.decodeFile(picturePath);
+            //Keep a copy
+            Bitmap originalMutable = originalImage.copy(originalImage.getConfig(), true);
+            splitImage(originalImage, 1024);
+        }
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     * <p/>
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    private void splitImage(Bitmap bitmap, int chunkNumbers) {
+
+        //For the number of rows and columns of the grid to be displayed
+        int rows, cols;
+
+        //For height and width of the small image chunks
+        int chunkHeight, chunkWidth;
+
+        //To store all the small image chunks in bitmap format in this list
+        ArrayList<Bitmap> chunkedImages = new ArrayList<Bitmap>(chunkNumbers);
+
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight(), true);
+
+        rows = cols = (int) Math.sqrt(chunkNumbers);
+        chunkHeight = bitmap.getHeight() / rows;
+        chunkWidth = bitmap.getWidth() / cols;
+
+        //x and y are the pixel positions of the image chunks
+        int yCoord = 0;
+        for (int x = 0; x < rows; x++) {
+            int xCoord = 0;
+            for (int y = 0; y < cols; y++) {
+                chunkedImages.add(Bitmap.createBitmap(scaledBitmap, xCoord, yCoord, chunkWidth, chunkHeight));
+                xCoord += chunkWidth;
+            }
+            yCoord += chunkHeight;
+        }
+
+        //Getting the grid view and setting an adapter to it
+        GridView grid = (GridView) findViewById(R.id.gridview);
+        grid.setAdapter(new ImageAdapter(this, chunkedImages));
+        grid.setNumColumns((int) Math.sqrt(chunkedImages.size()));
+    }
+}
---
+++ app/src/main/java/com/example/slartibartfast/mospho/Utilities/Utils.java
@@ -0,0 +1,48 @@
+package com.example.slartibartfast.mospho.Utilities;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+import java.util.ArrayList;
+
+/**
+ * Created by slartibartfast on 11/18/16.
+ */
+public class Utils {
+
+    /**
+     * Calculate the Red, Green and Blue pixel values of a given image.
+     * Get the total value of each of R, G, B and divide each by the
+     * total number of pixels.
+     *
+     * @param image is a Bitmap object.
+     * @return ArrayList<R, G, B> with each of R,G,B being Integers.
+     */
+    public ArrayList<Integer> getAverageDominantColourFromBitmap(Bitmap image) {
+        int width = image == null ? 0 : image.getWidth();
+        int height = image == null ? 0 : image.getHeight();
+        int numPixels = width * height;
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+
+        int[] pixels = new int[numPixels];
+        // Get all the pixels in the image and iterate over them
+        // to find their R,G,B values.
+        image.getPixels(pixels, 0, width, 0, 0, width, height);
+        for (int x = 0; x < numPixels; x++) {
+            red += Color.red(pixels[x]);
+            green += Color.green(pixels[x]);
+            blue += Color.blue(pixels[x]);
+        }
+
+        ArrayList<Integer> average = new ArrayList<Integer>();
+        average.add(red / numPixels);
+        average.add(green / numPixels);
+        average.add(blue / numPixels);
+        // Return the RGB average of the image.
+        return average;
+    }
+
+
+}
